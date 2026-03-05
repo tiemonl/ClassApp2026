d3388adf91188da3f8fa74e21f37d28bb6880c7a
tiemonl <github.lg4iy@simplelogin.fr>
2026-03-04 20:02:01 -0500

    added detail fragment and navigation between fragments

---
+++ app/src/main/java/edu/nku/classapp/CharacterDetailFragment.kt
@@ -0,0 +1,34 @@
+package edu.nku.classapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+class CharacterDetailFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(
+            R.layout.fragment_character_detail,
+            container,
+            false
+        )
+
+        if (arguments != null) {
+            val name = requireArguments().getString("name")
+            val age = requireArguments().getInt("age")
+
+
+            view.findViewById<TextView>(R.id.character_detail_name).text = name
+        }
+
+
+        return view
+    }
+}
--- app/src/main/java/edu/nku/classapp/RickAndMortyCharacterAdapter.kt
+++ app/src/main/java/edu/nku/classapp/RickAndMortyCharacterAdapter.kt
@@ -5,6 +5,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.bundleOf
+import androidx.fragment.app.commit
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 
@@ -15,13 +18,38 @@ class RickAndMortyCharacterAdapter(
         parent: ViewGroup,
         viewType: Int
     ): RickAndMortyCharacterViewHolder {
-        return RickAndMortyCharacterViewHolder(
-            LayoutInflater.from(parent.context).inflate(
-                R.layout.character_card_view,
-                parent,
-                false
-            )
+        val view = LayoutInflater.from(parent.context).inflate(
+            R.layout.character_card_view,
+            parent,
+            false
         )
+
+        return RickAndMortyCharacterViewHolder(view) { position ->
+            val character = characters[position]
+
+
+            val bundle = bundleOf(
+                "name" to character.name,
+                "age" to character.age
+            )
+
+
+            val detailFragment = CharacterDetailFragment()
+
+            detailFragment.arguments = bundle
+
+            val activity = view.context as AppCompatActivity
+
+            activity.supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                replace(
+                    R.id.fragment_container,
+                    detailFragment
+                )
+                addToBackStack(null)
+            }
+
+        }
     }
 
     override fun onBindViewHolder(
@@ -41,7 +69,17 @@ class RickAndMortyCharacterAdapter(
 
     override fun getItemCount() = characters.size
 
-    class RickAndMortyCharacterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    class RickAndMortyCharacterViewHolder(
+        itemView: View,
+        private val onCharacterClicked: (position: Int) -> Unit
+    ) : RecyclerView.ViewHolder(itemView) {
+
+        init {
+            itemView.setOnClickListener {
+                onCharacterClicked(adapterPosition)
+            }
+        }
+
         val characterImage: ImageView = itemView.findViewById(R.id.character_image)
         val characterName: TextView = itemView.findViewById(R.id.character_name)
         val characterPlanet: TextView = itemView.findViewById(R.id.character_planet)
